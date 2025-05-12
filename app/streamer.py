81272693a26bab0d85d6932b3ef5ecc522f51006
Yarramsetti Naresh <ynareshkalyan@gmail.com>
2025-05-12 06:08:54 +0530

    cr

--- app/streamer.py
+++ app/streamer.py
@@ -4,7 +4,7 @@ from app.config import RTSP_URL
 latest_frame = None
 
 def rtsp_reader():
-    print("starting RTSP reader", RTSP_URL)
+    print(f"starting RTSP reader,RTSP_URL:  {RTSP_URL}")
     global latest_frame
     cap = cv2.VideoCapture(RTSP_URL)
     print("RTSP reader started")
